89711d804483a95cba410ef02c3d7c942bc2b0f4
eduardoerthal <eduardoerthaloliveira@gmail.com>
2026-03-19 11:23:49 -0300

    Arrumei algumas coisas

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,7 +5,7 @@ import Entrar from "../pages/Entrar.jsx";
 import Cadastro from "../pages/Cadastro_inical.jsx";
 import Cadastro_resto from "../pages/Cadastro.jsx";
 import ChatPage from "../pages/chat/ChatPage.jsx";
-import Sobre from "../pages/Sobre.jsx";
+import Historia from "../pages/Historia.jsx";
 
 function Router() {
   return (
@@ -18,7 +18,7 @@ function Router() {
         <Route path="/cadastro_resto" element={<Cadastro_resto />} />
         <Route path="/cadastre_aqui" element={<Cadastro />} />
         <Route path="/yeBOT" element={ <ChatPage />} />
-        <Route path="/sobre" element={<Sobre /> } />
+        <Route path="/historia" element={<Historia /> } />
 
       </Routes>
     </BrowserRouter>
