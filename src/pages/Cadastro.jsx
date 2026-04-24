3631becc30fa1abb93110a0d8664c71074d27ade
eduardoerthal <eduardoerthaloliveira@gmail.com>
2026-04-24 12:32:25 -0300

    Alteração no campo sobre da navbar e arrumei a segunda parte do cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -4,12 +4,15 @@ import "../styles/Cadastro.css";
 function Cadastro_resto(){
     return(
         <div className="container">
-            <h1>Cadastro</h1>
-
-            <input className="campo" type="text" placeholder="CPF" />
-            <input className="campo" type="email" placeholder="E-mail" />
-            <input className="campo" type="password" placeholder="Senha" />
-            <button className="botao">Cadastrar</button>
+            
+      <div className="form">
+        <h1>Vamos continuar seu cadastro!</h1>
+        <p>Preencha os campos abaixo</p>
+        <input className="campo" type="text" placeholder="CPF" />
+        <input className="campo" type="email" placeholder="E-mail" />
+        <input className="campo" type="password" placeholder="Senha" />
+        <button className="botao">Cadastrar</button>
+      </div>
         </div>
     );
 }
